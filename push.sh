2a69ce7ebfc236acf10f49f588d05fbacfc35ab0
3siksfather <3siksfather@gmail.com>
2021-01-22 01:15:51 +0900

    ok

--- push.sh
+++ push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git add .
 git commit -m 'ok'
-git push --repo https://wspark:qkrwls9@github.com/wspark/sample-eap72.git
+#git push --repo https://wspark:qkrwls9@github.com/wspark/sample-eap72.git
+git push origin master
